f1557bd30956980db8114c3b117ae20d37b9ec31
Mohammad Wasil <mwasil@outlook.co.id>
2021-09-17 17:57:41 +0200

    Fixed typo

--- migrave_action_recognition/setup.py
+++ migrave_action_recognition/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['migrave_action_reconition',
+    packages=['migrave_action_recognition',
               'migrave_action_recognition_wrapper'],
     package_dir={'migrave_action_recognition': 'common/src/migrave_action_recognition',
                  'migrave_action_recognition_wrapper': 'ros/src/migrave_action_recognition_wrapper'}
