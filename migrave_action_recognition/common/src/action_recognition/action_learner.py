1ba5ae1f6126a48892ebfc68ea07980fb5456362
Intelwi <mlk04@wp.pl>
2022-10-19 13:29:38 +0200

    fix learning multiple actions processing issue

--- migrave_action_recognition/common/src/action_recognition/action_learner.py
+++ migrave_action_recognition/common/src/action_recognition/action_learner.py
@@ -11,12 +11,13 @@ from migrave_skeleton_tools_ros.skeleton_utils import JointUtils
 ntu_joint_map = [3, 2, 1, 0, 20, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18]
 
 class ActionLearner(object):
-    def __init__(self, action_model, save_path, nuitrack_skeleton_topic="/qt_nuitrack_app/skeletons", seq_size=50):
+    def __init__(self, action_model, save_path, nuitrack_skeleton_topic="/qt_nuitrack_app/skeletons", num_seq=25, seq_size=50):
 
         self.model = action_model
         self.save_path = save_path
 
         self.ske_seq = Queue(maxsize = seq_size)
+        self.num_seq = num_seq
         self.seq_size = seq_size
         self.seq_cnt = 0
 
@@ -27,12 +28,13 @@ class ActionLearner(object):
 
     def learn(self, action):
         skes_data = []
+        num_actions = action.num_actions
 
         rospy.loginfo('Sleeping for 3 seconds; get into position!')
-        self.qt_speech.publish('Get into position!')
+        #self.qt_speech.publish('Get into position!')
         rospy.sleep(3.)
 
-        while self.seq_cnt < 25*action.num_actions:
+        while self.seq_cnt < self.num_seq*num_actions:
             rospy.loginfo('Capturing data...')
             #self.qt_speech.publish('Capturing action sequence!')
             self.record = True
@@ -49,7 +51,7 @@ class ActionLearner(object):
             rospy.sleep(2.)
 
         rospy.loginfo('Preparing Data...')
-        trn_data, val_data = self.prepare_data(skes_data, action.num_actions)
+        trn_data, val_data = self.prepare_data(skes_data, num_actions)
         np.savez(self.save_path + '/test.npz', x_train=trn_data['x'], y_train=trn_data['y'], x_test=val_data['x'], y_test=val_data['y'])
         return True
         self.model.train(action, trn_data, val_data)
@@ -99,7 +101,14 @@ class ActionLearner(object):
                self.record = True
 
     def prepare_data(self, skes_data, num_actions):
-        action_data = np.zeros((len(skes_data), self.seq_size, 19*6), dtype=np.float32)
+        n_train = int(self.num_seq*.8)
+        n_val = int(self.num_seq*.2)
+        y_train = []
+        y_val = []
+
+        learned_data = np.zeros((self.num_seq*num_actions, self.seq_size, 19*6), dtype=np.float32)
+        train_data = np.zeros((n_train*num_actions, self.seq_size, 19*6), dtype=np.float32)
+        val_data = np.zeros((n_val*num_actions, self.seq_size, 19*6), dtype=np.float32)
 
         for idx, ske_data in enumerate(skes_data):
             num_frames = ske_data.shape[0]
@@ -112,22 +121,19 @@ class ActionLearner(object):
             for f in range(num_frames):
                 ske_joints[f] -= np.tile(origin, 19)
 
-            action_data[idx, :num_frames] = np.hstack((ske_joints, np.zeros_like(ske_joints)))
+            learned_data[idx, :num_frames] = np.hstack((ske_joints, np.zeros_like(ske_joints)))
 
-        N, T, J = action_data.shape
-        x = action_data.reshape((N, T, 2, int(J/6), 3)).transpose(0, 4, 1, 3, 2)
+        for i in range(num_actions):
+            train_data[n_train*i:n_train*(i+1)] = learned_data[self.num_seq*i:(self.num_seq*i + n_train)]
+            y_train.extend([len(self.model.actions)+i]*n_train)
+            val_data[n_val*i:n_val*(i+1)] = learned_data[(self.num_seq*i + n_train):self.num_seq*(i+1)]
+            y_val.extend([len(self.model.actions)+i]*n_val)
 
-        x_train = x[:20]
-        x_val = x[20:25]
-        y_train = [len(self.model.actions)]*20
-        y_val = [len(self.model.actions)]*5
+        N, T, J = train_data.shape
+        x_train = train_data.reshape((N, T, 2, int(J/6), 3)).transpose(0, 4, 1, 3, 2)
 
-        if num_actions > 1:
-            for i in range(1, num_actions):
-                x_train = np.hstack((x_train, x[25*i:25*i+20]))
-                x_val = np.hstack((x_val, x[25*i+20:25*(i+1)]))
-                y_train.append([len(self.model.actions)+i]*20)
-                y_val.append([len(self.model.actions)+i]*5)
+        N, T, J = val_data.shape
+        x_val = val_data.reshape((N, T, 2, int(J/6), 3)).transpose(0, 4, 1, 3, 2)
 
         trn_data = {'x': x_train, 'y': y_train}
         val_data = {'x': x_val, 'y': y_val}
